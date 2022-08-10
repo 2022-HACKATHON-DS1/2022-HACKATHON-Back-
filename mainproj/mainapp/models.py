ac3aa3b4c643db8969d4a8ae254fb14676909626
jiinkyung <jinkyung010310@gmail.com>
2022-08-10 23:32:37 +0900

    add settings

--- mainproj/mainapp/models.py
+++ mainproj/mainapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    image = models.ImageField(blank=True, upload_to='my_image')
+    date = models.DateField()
+
+    def __str__(self):
+        return self.title
